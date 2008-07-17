68fdd1e39e07298828f0452dc5caab26806b508a
esa.karjalainen <esa.karjalainen@abe21748-ea51-0410-af0e-c35c4496dcea>
2008-07-17 22:02:54 +0000

    initial stub or getting used to svn repository
    git-svn-id: https://jeshmup.googlecode.com/svn/trunk@5 abe21748-ea51-0410-af0e-c35c4496dcea

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "SDL.h"
+
+int main( int argc, char *argv[] )
+{
+	if(SDL_Init(0) <0){
+		std::cout<<"Could not init SDL:"<<SDL_GetError()<<"\n";
+		exit(1);
+	}	
+	return 0;
+}
