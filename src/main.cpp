cd711ed4123c1af5bb20cecf55bc777cc8cf08f4
esa.karjalainen <esa.karjalainen@abe21748-ea51-0410-af0e-c35c4496dcea>
2008-08-16 20:02:39 +0000

    Readme + UnitTest++ and Makefile tweaking
    git-svn-id: https://jeshmup.googlecode.com/svn/trunk@11 abe21748-ea51-0410-af0e-c35c4496dcea

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include "SDL.h"
+#include "UnitTest++.h"
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
+#define SCREEN_BITS 16
+
+int init_screen(void);
 
 int main( int argc, char *argv[] )
 {
-	if(SDL_Init(0) <0){
+	init_screen();
+
+	if(SDL_Init(SDL_INIT_VIDEO) <0){
 		std::cout<<"Could not init SDL:"<<SDL_GetError()<<"\n";
 		exit(1);
-	}	
+	}
 	SDL_Quit();
 	return 0;
 }
+
+
+// does nothing, reserve screen elsewhere, idiot!
+int init_screen(void)
+{
+	SDL_Surface *screen = NULL ;
+	SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BITS, SDL_ANYFORMAT);
+	return 0;
+}
+
